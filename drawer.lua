24c0f64842415edf97098a060bfa59b1da6ed711
Yan Paiham <y.paiha@gmail.com>
2020-08-28 21:29:51 +0200

    initial commit

---
+++ drawer.lua
@@ -0,0 +1,9 @@
+component = require("component)
+drawer = component.inventory_controller
+side = 3
+
+dInfo = drawer.getInventorySize(side)
+
+for i=0,drawer.getInventorySize(side) do
+    print(drawer.getStackInInternalSlot(i))
+end
