6dce358160c32b615d28678256c4a9342b9fdfcb
Yan Paiham <y.paiha@gmail.com>
2020-08-28 22:32:19 +0200

    WIP

--- drawer.lua
+++ drawer.lua
@@ -6,7 +6,7 @@ dInfo = drawer.getInventorySize(side)
 
 for i=1,drawer.getInventorySize(side) do
     if drawer.getStackInSlot(side, i) ~= nil then
-        drawer.getStackInSlot(side, i).name
+        print(drawer.getStackInSlot(side, i))
     end
 end
 
